c84d273416e787e598535843db39b4c5cc78e941
Rafael Villegas Michel <rvillegasm@eafit.edu.co>
2020-10-23 11:17:49 -0500

    Rendering a basic triangle, to see if everything works

--- Quasar/src/Platform/OpenGL/OpenGLContext.cpp
+++ Quasar/src/Platform/OpenGL/OpenGLContext.cpp
@@ -1,6 +1,7 @@
 #include "OpenGLContext.hpp"
 
 #include "Quasar/Core.hpp"
+#include "Quasar/Log.hpp"
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -19,6 +20,11 @@ namespace Quasar
         glfwMakeContextCurrent(m_WindowHandle);
         int status = gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
         QS_CORE_ASSERT(status, "Failed to initialize glad!");
+
+        QS_CORE_INFO("OpenGL Info:");
+        QS_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+        QS_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+        QS_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
     }
 
     void OpenGLContext::swapBuffers()
--- Quasar/src/Quasar/Application.hpp
+++ Quasar/src/Quasar/Application.hpp
@@ -19,6 +19,9 @@ namespace Quasar
         ImGuiLayer *m_ImGuiLayer;
         bool m_Running = true;
         LayerStack m_LayerStack;
+
+        unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+
         static Application *s_Instance;
 
         bool onWindowClosed(WindowCloseEvent &e);
