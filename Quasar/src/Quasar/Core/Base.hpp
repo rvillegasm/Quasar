0b6732b5933e1259f726d81a10e52ec3d411a004
Rafael Villegas Michel <rvillegasm@eafit.edu.co>
2021-02-02 13:30:40 -0500

    Modifying assertions to make the message optional

--- Quasar/src/Quasar/Core/Base.hpp
+++ Quasar/src/Quasar/Core/Base.hpp
@@ -4,6 +4,7 @@
 #include "Quasar/Core/PlatformDetection.hpp"
 
 #include <memory>
+#include <filesystem>
  
 #ifdef QS_DEBUG
     #if defined(QS_PLATFORM_WINDOWS)
@@ -19,29 +20,24 @@
     #define QS_DEBUGBREAK()
 #endif
 
+#define QS_EXPAND_MACRO(x) x
+#define QS_STRINGIFY_MACRO(x) #x
+
 #ifdef QS_ENABLE_ASSERTS
-    #include <cstdlib>
-    #define QS_ASSERT(x, ...)                                   \
-        {                                                       \
-            if (!(x))                                           \
-            {                                                   \
-                QS_ERROR("Assertion failed: {0}", __VA_ARGS__); \
-                QS_DEBUGBREAK();                                \
-                std::exit(EXIT_FAILURE);                        \
-            }                                                   \
-        }
-    #define QS_CORE_ASSERT(x, ...)                                   \
-        {                                                            \
-            if (!(x))                                                \
-            {                                                        \
-                QS_CORE_ERROR("Assertion failed: {0}", __VA_ARGS__); \
-                QS_DEBUGBREAK();                                     \
-                std::exit(EXIT_FAILURE);                             \
-            }                                                        \
-        }
+    #define QS_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if (!(check)) { QS##type##ERROR(msg, __VA_ARGS__); QS_DEBUGBREAK(); } }
+    
+    #define QS_INTERNAL_ASSERT_WITH_MSG(type, check, ...) QS_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
+    #define QS_INTERNAL_ASSERT_NO_MSG(type, check) QS_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", QS_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
+
+    #define QS_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
+    #define QS_INTERNAL_ASSERT_GET_MACRO(...) QS_EXPAND_MACRO( QS_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, QS_INTERNAL_ASSERT_WITH_MSG, QS_INTERNAL_ASSERT_NO_MSG) )
+
+    // Accepts the condition first, and then an optional message parameter
+    #define QS_ASSERT(...) QS_EXPAND_MACRO( QS_INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(_, __VA_ARGS__) )
+    #define QS_CORE_ASSERT(...) QS_EXPAND_MACRO( QS_INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(_CORE_, __VA_ARGS__) )
 #else
-    #define QS_ASSERT(x, ...)
-    #define QS_CORE_ASSERT(x, ...)
+    #define QS_ASSERT(...)
+    #define QS_CORE_ASSERT(...)
 #endif
 
 #define BIT(x) (1 << x)
