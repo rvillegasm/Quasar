694febb4ff44df7d763a4784632a6600faa04ae0
Rafael Villegas Michel <rvillegasm@eafit.edu.co>
2021-02-04 11:38:56 -0500

    Removing unnecessary member inside the FS

--- Quasar/src/Quasar/System/FileSystem.hpp
+++ Quasar/src/Quasar/System/FileSystem.hpp
@@ -10,7 +10,6 @@ namespace Quasar
     {
     private:
         static std::filesystem::path s_CurrentPath;
-        static std::filesystem::path s_LoggingDirectoryPath;
 
     public:
         static std::string getAbsolutePath(std::string_view filepath);
