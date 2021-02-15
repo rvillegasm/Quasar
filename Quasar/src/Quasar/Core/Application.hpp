0d2a713d5fe4f5be28860d439a488b0412004f3e
Rafael Villegas <rvillegasm@eafit.edu.co>
2021-02-15 11:34:33 -0500

    Code Maintenance
    * Changing std::unique_ptr to Quasar::Scope in Application.
    * Updating the texture shaders to use more modern standards.

--- Quasar/src/Quasar/Core/Application.hpp
+++ Quasar/src/Quasar/Core/Application.hpp
@@ -18,7 +18,7 @@ namespace Quasar
     class Application
     {
     private:
-        std::unique_ptr<Window> m_Window;
+        Scope<Window> m_Window;
         ImGuiLayer *m_ImGuiLayer;
         bool m_Running = true;
         bool m_Minimized = false;
